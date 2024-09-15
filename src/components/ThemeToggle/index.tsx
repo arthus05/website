73332db0e89745ea518752c621d4968ef4584612
Arthus Vinicius <arthusvinicius.dev@gmail.com>
2024-09-14 22:42:36 -0300

    wip: project deps update

--- src/components/ThemeToggle/index.tsx
+++ src/components/ThemeToggle/index.tsx
@@ -1,24 +1,31 @@
-import { ThemeContext } from '@/providers/theme'
-import { useContext } from 'react'
+import { ThemeContext } from "@/providers/theme";
+import { useContext } from "react";
 
-import { MdLightMode, MdModeNight } from 'react-icons/md'
+import { MdLightMode, MdModeNight } from "react-icons/md";
 
 const ThemeToggle = () => {
-  const { theme, updateTheme } = useContext(ThemeContext)
+  const { theme, updateTheme } = useContext(ThemeContext);
 
   return (
     <>
-      <label className='cursor-pointer'>
+      <label className="cursor-pointer">
         <input
           type="checkbox"
-          checked={theme === 'dark'}
-          onChange={e => updateTheme && updateTheme(e.target.checked ? 'dark' : 'light')}
+          checked={theme === "dark"}
+          onChange={(e) =>
+            updateTheme && updateTheme(e.target.checked ? "dark" : "light")
+          }
           className="hidden"
-          />
-          { theme === 'light' ? <MdModeNight size={32} /> : <MdLightMode size={32} /> }
+        />
+        {theme === "light" ? (
+          <MdModeNight size={32} />
+        ) : (
+          <MdLightMode size={32} />
+        )}
       </label>
     </>
-  )
-}
+  );
+};
 
-export default ThemeToggle
+export default ThemeToggle;
+
